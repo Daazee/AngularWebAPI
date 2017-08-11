3d2031aba4ecb2f3de48e47291ba090be566fee4
Chukwuma Precious <pchukwuma@eminenttechnology.com>
2017-08-12 00:24:51 +0100

    Added scripts for form wizard, and AddEmployee.html

--- src/AngularWebAPI.Web/app/Employee/AddEmployee/AddEmployee.js
+++ src/AngularWebAPI.Web/app/Employee/AddEmployee/AddEmployee.js
@@ -1,29 +1,4 @@
-﻿//(function () {
-//    "use strict";
-//    var module = angular.module("employeeManagement");
-
-//    function controller($http) {
-//        debugger;
-//        var model = this;
-//        model.FirstName = "this";
-//        model.addEmployee = function () {
-//            alert(model.FirstName)
-//            return $http.post("")
-//            .then(function (response) {
-//                return response.data;
-//            })
-//        };
-
-
-//    }
-//    module.component("createEmployee", {
-//        templateUrl: "app/Employee/AddEmployee/AddEmployee.html",
-//        controllerAs: "model",
-//        controller: ["$http", controller]
-//    });
-//}());
-
-(function () {
+﻿(function () {
     "use strict";
     var module = angular.module("employeeManagement");
 
@@ -32,10 +7,8 @@
         controllerAs: "model",
         controller: function(){
             var model = this;
-
             model.addEmployee = function () {
-
-            alert("My fullname is " + model.Lastname + ' ' + model.Firstname);
+            alert("My fullname is " + model.lastname + ' ' + model.firstname);
         };
 
         }
